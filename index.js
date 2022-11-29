43b34af835bf760c690b75cfa1e1483c2c7efea2
Canh Nguyen <57476449+nvc-page@users.noreply.github.com>
2022-11-29 15:02:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $("style").append("/*--------------- footer section --------------- */.collab{text-align: right;}.collab p{font-weight: lighter !important; margin-bottom: 20px;}.hr{background: rgba(255, 255, 255, 0.2); height: 1px;}.info h4{font-size: 18px; font-weight: lighter;}.info p{color: grey; font-weight: lighter;}.info li{font-weight: lighter; color: #fff; font-size: 18px; padding-left: 20px;}#fb::before{display: inline-block; content: ''; border-radius: 100%; height: 4px; width: 4px; margin-right: 6px; background: #3b5998;}#ig::before{display: inline-block; content: ''; border-radius: 100%; height: 4px; width: 4px; margin-right: 6px; background: #fcaf45;}#tw::before{display: inline-block; content: ''; border-radius: 100%; height: 4px; width: 4px; margin-right: 6px; background: #55acee;}#yt::before{display: inline-block; content: ''; border-radius: 100%; height: 4px; width: 4px; margin-right: 6px; background: #c4302b;}#media, #address{text-align: right;}#media ul{list-style: none;}#media ul li{display: inline-block;}@media(max-width: 768px){.collab, #personal, #media, #address{text-align: center;}.info ul{margin: 0 0 0 -22px; padding: 0;}};")
 
     $(".nav-bar").append('<nav> <span id="brand"> <a href="index.html">H Ξ I S Ɔ Λ</a> </span> <ul id="menu"> <li><a href="index.html">home<span>.</span></a></li><li><a href="work.html">work<span>.</span></a></li><li><a href="about.html">about me<span>.</span></a></li><li><a href="contact.html">contact<span>.</span></a></li></ul> <div id="toggle"> <div class="span">menu</div></div></nav> <div id="resize"> <div class="close-btn">close</div><ul id="menu"> <li><a href="index.html">home<span>.</span></a></li><li><a href="work.html">work<span>.</span></a></li><li><a href="about.html">about me<span>.</span></a></li><li><a href="contact.html">contact<span>.</span></a></li></ul> </div>')
-
+    $("body").prepend('<div class="curzr" hidden><div class="circle"></div><div class="dot"></div></div>')
 
     // navigation
 
@@ -43,7 +43,124 @@ $(document).ready(function(){
     // nav animation ends
 
     new WOW().init();
+
+    // cursor
+
+    class BigCircle {
+        constructor() {
+          this.root = document.body
+          this.cursor = document.querySelector(".curzr")
+          this.circle = document.querySelector(".curzr .circle")
+          this.dot = document.querySelector(".curzr .dot")
+      
+          this.pointerX = 0
+          this.pointerY = 0
+          this.cursorSize = 64
+      
+          this.circleStyle = {
+            boxSizing: 'border-box',
+            position: 'fixed',
+            top: `${ this.cursorSize / -2 }px`,
+            left: `${ this.cursorSize / -2 }px`,
+            zIndex: '2147483647',
+            width: `${ this.cursorSize }px`,
+            height: `${ this.cursorSize }px`,
+            backgroundColor: '#fff0',
+            borderRadius: '50%',
+            transition: '500ms, transform 100ms',
+            userSelect: 'none',
+            pointerEvents: 'none'
+          }
+      
+          this.dotStyle = {
+            boxSizing: 'border-box',
+            position: 'fixed',
+            zIndex: '2147483647',
+            width: '6px',
+            height: '6px',
+            backgroundColor: '#fffd',
+            borderRadius: '50%',
+            userSelect: 'none',
+            pointerEvents: 'none',
+            transition: '250ms, transform 75ms'
+          }
+      
+          if (CSS.supports("backdrop-filter", "invert(1) grayscale(1)")) {
+            this.circleStyle.backdropFilter = 'invert(1) grayscale(1)'
+            this.circleStyle.backgroundColor = '#fff0'
+            this.dotStyle.backdropFilter = 'invert(1) grayscale(1)'
+            this.dotStyle.backgroundColor = '#fff0'
+          } else {
+            this.circleStyle.backgroundColor = '#000'
+            this.circleStyle.opacity = '0.75'
+            this.dotStyle.backgroundColor = '#fff'
+            this.dotStyle.opacity = '0.75'
+          }
+      
+          this.init(this.circle, this.circleStyle)
+          this.init(this.dot, this.dotStyle)
+        }
+      
+        init(el, style) {
+          Object.assign(el.style, style)
+          this.cursor.removeAttribute("hidden")
+          
+          document.body.style.cursor = 'none'
+          document.body.querySelectorAll("button, label, input, textarea, select, a").forEach((el) => {
+            el.style.cursor = 'inherit'
+          })
+        }
+      
+        move(event) {
+          this.pointerX = event.pageX
+          this.pointerY = event.pageY + this.root.getBoundingClientRect().y
+        
+          this.circle.style.transform = `translate3d(${this.pointerX}px, ${this.pointerY}px, 0)`
+          this.dot.style.transform = `translate3d(calc(-50% + ${this.pointerX}px), calc(-50% + ${this.pointerY}px), 0)`
+      
+          if (event.target.localName === 'button' || 
+              event.target.localName === 'a' || 
+              event.target.onclick !== null ||
+              event.target.className.includes('curzr-hover')) {
+            this.hover()
+          }
+        }
+      
+        hover() {
+          this.circle.style.transform += ` scale(1.5)`
+        }
+      
+        click() {
+          this.circle.style.transform += ` scale(0.75)`
+          setTimeout(() => {
+            this.circle.style.transform = this.circle.style.transform.replace(` scale(0.75)`, '')
+          }, 35)
+        }
+      
+        remove() {
+          this.circle.remove()
+          this.dot.remove()
+        }
+      }
+      
+      (() => {
+        const cursor = new BigCircle()
+        if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+          document.onmousemove = function (event) {
+            cursor.move(event)
+          }
+          document.onclick = function () {
+            cursor.click()
+          }
+        } else {
+          cursor.remove()
+        }
+        
+      })()
+    //----------------
+    
 });
+
 
 
 
